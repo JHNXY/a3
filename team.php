6ca973bf1966f9c7aa90ac8250b5866479d79d98
JHNXY <s3652190@student.rmit.edu.au>
2021-11-01 23:32:18 +0530

    member section style

--- team.php
+++ team.php
@@ -37,9 +37,8 @@ $api_key = '312HqH0OJm8sNBZqjOpCgJtteK4JXTbn979qHyKmpSE';
         <p class="designation">PRODUCTOR</p>
         <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dicta facilis veniam illum perferendis ducimus voluptatibus consectetur ipsum. Quos excepturi totam, omnis tempora vero nam?</span>
         <div class="link">
-            <img src="<?php echo "$base_url?&z=13&ci=Perth&&&w=400&apiKey=$api_key" ?>" alt="">
         </div>
-        <img class="map" src="" alt="Perth">
+        <img src="<?php echo "$base_url?&z=13&ci=Perth&&&w=400&apiKey=$api_key" ?>" alt="">
     </div>
 </div>
 <?php
