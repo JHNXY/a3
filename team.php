18fe4c182d330076e50b2f09124f9d75552723db
JHNXY <s3652190@student.rmit.edu.au>
2021-11-01 23:09:42 +0530

    added map images from rest api

--- team.php
+++ team.php
@@ -1,6 +1,8 @@
 <?php
 require_once('./partials/header.php');
 require_once('./partials/nav.php');
+$base_url = 'https://image.maps.ls.hereapi.com/mia/1.6/mapview';
+$api_key = '312HqH0OJm8sNBZqjOpCgJtteK4JXTbn979qHyKmpSE';
 ?>
 <div class="container">
     <div class="item">
@@ -13,7 +15,7 @@ require_once('./partials/nav.php');
         <div class="link">
             <img src="" alt="">
         </div>
-        <img class="map" src="" alt="Sydney">
+        <img class="map" src="<?php echo "$base_url?&z=13&ci=Sydney&&&w=400&apiKey=$api_key" ?>" alt="Sydney">
     </div>
     <div class="item">
         <div class="avataar-container">
@@ -25,7 +27,7 @@ require_once('./partials/nav.php');
         <div class="link">
             <img src="" alt="">
         </div>
-        <img class="map" src="" alt="Melbourne">
+        <img class="map" src="<?php echo "$base_url?&z=13&ci=Melbourne&&&w=400&apiKey=$api_key" ?>" alt="Melbourne">
     </div>
     <div class="item">
         <div class="avataar-container">
@@ -35,7 +37,7 @@ require_once('./partials/nav.php');
         <p class="designation">PRODUCTOR</p>
         <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dicta facilis veniam illum perferendis ducimus voluptatibus consectetur ipsum. Quos excepturi totam, omnis tempora vero nam?</span>
         <div class="link">
-            <img src="" alt="">
+            <img src="<?php echo "$base_url?&z=13&ci=Perth&&&w=400&apiKey=$api_key" ?>" alt="">
         </div>
         <img class="map" src="" alt="Perth">
     </div>
